334e8981af0b6a84a635b9b1798aea91a79b04b6
Penguin <say8425@gmail.com>
2020-01-31 16:40:30 +0900

    test send sms

--- test/sens_sms_test.rb
+++ test/sens_sms_test.rb
@@ -1,11 +1,14 @@
 require 'test_helper'
 
 class SensSmsTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    assert_not_nil ::SensSms::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+  def test_send_sms
+    SensSms::Client.configure(access_key: ENV['access_key'],
+                              secret_key: ENV['secret_key'],
+                              service_id: ENV['service_id'])
+    sens_sms = SensSms::Client.new
+    assert(sens_sms.deliver(type: :sms,
+                            from_number: ENV['from_number'],
+                            to_numbers: ENV['to_number'],
+                            message: '키스의 고유 조건은 입술끼리 만나야 하고 특별한 기술은 필요치 않다.'))
   end
 end
