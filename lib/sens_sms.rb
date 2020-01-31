6dd5f521b2b39dd5e4a05cfe9b343218156ccfad
Penguin <say8425@gmail.com>
2020-01-31 16:41:12 +0900

    move configure variable to class variable

--- lib/sens_sms.rb
+++ lib/sens_sms.rb
@@ -1,6 +1,7 @@
+require 'http'
 require 'sens_sms/version'
+require 'sens_sms/configuration'
+require 'sens_sms/client'
 
 module SensSms
-  class Error < StandardError; end
-  # Your code goes here...
 end
--- lib/sens_sms/client.rb
+++ lib/sens_sms/client.rb
@@ -1,52 +1,102 @@
 module SensSms
   class Client
-    attr_reader :status, :errors, :request_time
+    attr_reader :response, :status, :errors, :request_time, :timestamp
 
-    def initialize(opts = {})
-      @access_key = opts[:access_key]
-      @secret_key = opts[:secret_key]
-      @service_id = opts[:service_id]
+    def deliver(type:, from_number:, to_numbers:, subject: nil, message:)
+      case type.to_sym
+      when :sms then post_sms(from_number: from_number, to_numbers: to_numbers, message: message)
+      when :lms then post_lms(from_number: from_number, to_numbers: to_numbers, subject: subject, message: message)
+      else raise "Invalid message type: #{type}"
+      end
     end
 
-    def send_sms(sender_number:, receiver_number:, message:)
-      @timestamp = DateTime.current.strftime('%Q')
-      response = HTTP.headers(ncloud_sens_api_header)
-                     .post(ncloud_sens_api_url,
-                           json: { type: 'SMS',
-                                   contentType: 'COMM',
-                                   countryCode: '82',
-                                   from: sender_number,
-                                   content: message,
-                                   messages: [{ to: receiver_number }] })
+    def post_sms(from_number:, to_numbers:, message:)
+      @timestamp = current_timestamp
+      @response = HTTP.headers(ncloud_sens_api_header)
+                      .post(ncloud_sens_api_url,
+                            json: { type: :SMS,
+                                    contentType: :COMM,
+                                    countryCode: '82',
+                                    from: from_number,
+                                    content: message,
+                                    messages: parsed_messages(to_numbers) })
       @status = response.status
-      if response.status.success?
-        @request_time = Time.zone.parse(response.parse['requestTime'])
-        true
-      else
-        @errors = response.parse['errors'] if response.parse['errors'].present?
-        @errors = [response.parse['error']['message']] if response.parse['error'].present?
-        false
-      end
+      parse_response
+    end
+
+    def post_lms(from_number:, to_numbers:, subject:, message:)
+      @timestamp = current_timestamp
+      @response = HTTP.headers(ncloud_sens_api_header)
+                      .post(ncloud_sens_api_url,
+                            json: { type: :LMS,
+                                    contentType: :COMM,
+                                    countryCode: '82',
+                                    from: from_number,
+                                    subject: subject,
+                                    content: message,
+                                    messages: parsed_messages(to_numbers) })
+      @status = response.status
+      parse_response
+    end
+
+    def self.configure(access_key:, service_id:, secret_key:)
+      @@access_key = access_key
+      @@service_id = service_id
+      @@secret_key = secret_key
     end
 
     private
 
+    def parse_response
+      if status.success?
+        @request_time = Time.parse(response.parse['requestTime'])
+      else
+        parse_errors
+      end
+      status.success?
+    end
+
+    def parse_errors
+      @errors = response.parse['errors'] if response.parse.key? 'errors'
+      @errors = [response.parse['error']['message']] if response.parse.key? 'error'
+    end
+
+    def current_timestamp
+      (Time.now.to_f.round(3) * 1000).to_i
+    end
+
     def ncloud_sens_api_url
-      @ncloud_sens_api_url ||= "https://sens.apigw.ntruss.com/sms/v2/services/#{@service_id}/messages"
+      @ncloud_sens_api_url ||= "https://sens.apigw.ntruss.com/sms/v2/services/#{service_id}/messages"
     end
 
     def ncloud_sens_api_header
       { 'Content-Type': 'application/json; charset=utf-8',
         'x-ncp-apigw-timestamp': @timestamp,
         'x-ncp-apigw-signature-v2': signature,
-        'x-ncp-iam-access-key': @access_key }
+        'x-ncp-iam-access-key': access_key }
     end
 
     def signature
-      sms_uri = "/sms/v2/services/#{@service_id}/messages".freeze
-      data = "POST #{sms_uri}\n#{@timestamp}\n#{@access_key}".freeze
+      sms_uri = "/sms/v2/services/#{service_id}/messages".freeze
+      data = "POST #{sms_uri}\n#{@timestamp}\n#{access_key}".freeze
 
-      Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), @secret_key, data)).strip
+      Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), secret_key, data)).strip
+    end
+
+    def parsed_messages(to_numbers)
+      Array(to_numbers).map { |to_number| { to: to_number } }
+    end
+
+    def access_key
+      @@access_key
+    end
+
+    def secret_key
+      @@secret_key
+    end
+
+    def service_id
+      @@service_id
     end
   end
 end
